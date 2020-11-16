f28b88c46f3d7ed2936ee433d59093fc3b553d25
arianerocha4 <arianerocha4@hotmail.com>
2020-11-15 23:52:01 -0300

    alteracoes

--- Controle/while.js
+++ Controle/while.js
@@ -5,4 +5,9 @@ function getInteiroAleatorioEntre(min, max) {
 
 let opcao = 0
 
-while ()
+while (opcao != -1) {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}`)
+}
+
+console.log('Até a próxima')
