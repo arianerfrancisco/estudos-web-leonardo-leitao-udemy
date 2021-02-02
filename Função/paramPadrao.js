6e23aa7c8233a37a787d2165a1da70ac3b02e3d9
arianerochaf@gmail.com <72419533+arianerfrancisco@users.noreply.github.com>
2021-02-02 16:49:29 -0300

    arrow function

--- Função/paramPadrao.js
+++ Função/paramPadrao.js
@@ -6,6 +6,6 @@ function soma (a= 1, b=1, c= 1) {
 console.log(soma(2,2,2))
 console.log(soma())
 
-// Ex: Operador Condicional Ternário
+// Ex: Operador Condicional Ternários
 a = 10 > 2  ? 'Sim' : 'Não'
 console.log(a)
