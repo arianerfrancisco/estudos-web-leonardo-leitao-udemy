7cb4fcf8c78fa530884238d4315b7559fe8436c3
arianerocha4 <arianerocha4@hotmail.com>
2021-08-28 10:59:29 -0300

    alteracoes

--- Node/entradaESaida.js
+++ Node/entradaESaida.js
@@ -1,5 +1,8 @@
 // Ao passar dados pelo terminal, eles podem ser interpretados pelo process
-const anonimo = process.argv.indexOf('-a') !== -1 // estamos procurando a flag -a
+const anonimo = process.argv.indexOf('-a') !== -1 // estamos procurando a flag -a. 
+/*Para executar esse código, temos que chamar  o arquivo dentro do terminal
+ dentro da pasta correta colocar: node ./entradaESaida.js -a > colocando esse valor o retorno será true 
+caso contrário será false e solicitará o nome devido aos códigos abaixo. */
 console.log(anonimo)
 // console.log(anonimo)
 
@@ -8,7 +11,7 @@ if(anonimo) {
     process.exit()
 } else {
     process.stdout.write('Informe o seu nome: ')
-    process.stdin.on('data', data => {
+    process.stdin.on('data', data => { // evento data é quando existe a entrada a partir do teclado-TECLA ENTER
         const nome = data.toString().replace('\n', '')
 
         process.stdout.write(`Fala ${nome}!!\n`)
